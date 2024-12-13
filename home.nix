9cefbb24e24ada65fba514dcc32ee1ed6bc1a34a
Joshuahuahua <joshuahollander2001@gmail.com>
2024-12-13 16:29:31 +0800

    Add tree

--- home.nix
+++ home.nix
@@ -37,6 +37,7 @@ in
     gcc
     just
     neofetch
+    tree
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
